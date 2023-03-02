e8223e8cfdef7b8fef2f6568a55cd20084d4bf91
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2023-03-02 03:26:57 -0500

    eh

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -2,8 +2,8 @@ import { Listbox, Transition } from "@headlessui/react"
 import { IconCheck, IconMoonFilled, IconSunFilled } from "@tabler/icons-react"
 import { useCallback, useMemo, useState } from "react"
 
-const themes = ["system", "dark", "light"] as const
-type theme = (typeof themes)[number]
+const THEMES = ["system", "dark", "light"] as const
+type theme = (typeof THEMES)[number]
 
 const ThemeSelect = () => {
   const [selectedTheme, setSelectedTheme] = useState<theme>(
@@ -35,46 +35,49 @@ const ThemeSelect = () => {
   }, [])
 
   return (
-    <Listbox value={selectedTheme} onChange={handleChange}>
-      <div>
-        <Listbox.Button
-          aria-label="Select theme"
-          className="text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-white"
-        >
-          <Icon size={20} stroke={3} />
-        </Listbox.Button>
-        <Transition
-          enter="transition duration-100 ease-out"
-          enterFrom="transform scale-95 opacity-0"
-          enterTo="transform scale-100 opacity-100"
-          leave="transition duration-75 ease-out"
-          leaveFrom="transform scale-100 opacity-100"
-          leaveTo="transform scale-95 opacity-0"
-        >
-          <Listbox.Options className="absolute right-0 mt-2 rounded bg-white p-1 shadow-md dark:bg-gray-800">
-            {themes.map((theme) => (
-              <Listbox.Option key={theme} value={theme} className="relative">
-                {({ active, selected }) => (
-                  <div
-                    className={`flex cursor-pointer items-center justify-between rounded px-3 py-1 pr-10 capitalize ${
-                      active
-                        ? "bg-gray-100 text-black dark:bg-gray-700 dark:text-white"
-                        : "text-gray-700 dark:text-gray-300"
-                    }`}
-                  >
-                    {theme}
-                    {selected && (
-                      <span className="absolute right-0 pr-3">
-                        <IconCheck size={16} stroke={3} />
-                      </span>
-                    )}
-                  </div>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
-      </div>
+    <Listbox value={selectedTheme} onChange={handleChange} as="div">
+      <Listbox.Button aria-label="Select theme">
+        <Icon
+          size={20}
+          stroke={3}
+          className={`${
+            selectedTheme === "system"
+              ? "text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-white"
+              : "text-amber-600 hover:text-amber-500"
+          }`}
+        />
+      </Listbox.Button>
+      <Transition
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Listbox.Options className="absolute top-4 right-0 mt-2 rounded-lg border border-gray-200 bg-amber-50 p-2 shadow-md dark:border-0 dark:bg-gray-800">
+          {THEMES.map((theme) => (
+            <Listbox.Option key={theme} value={theme} className="relative">
+              {({ active, selected }) => (
+                <div
+                  className={`flex cursor-pointer items-center justify-between rounded-lg px-3 py-1 pr-10 capitalize ${
+                    active
+                      ? "bg-white text-black shadow-md dark:bg-gray-700 dark:text-white"
+                      : "text-gray-700 dark:text-gray-300"
+                  }`}
+                >
+                  {theme}
+                  {selected && (
+                    <span className="absolute right-0 pr-3">
+                      <IconCheck size={16} stroke={3} />
+                    </span>
+                  )}
+                </div>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Transition>
     </Listbox>
   )
 }
